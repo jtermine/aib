5e8347f2c8cd9162a151bee5cdbd026c881ea271
Joseph Termine <joe@termine.com>
2021-08-24 07:12:24 -0400

    jmeter

--- aib.ps1
+++ aib.ps1
@@ -14,11 +14,10 @@ Start-Sleep -Seconds 10
 Invoke-WebRequest -Uri 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1.3/npp.8.1.3.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
 Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
 
-#Start sleep
-Start-Sleep -Seconds 10
+#Install JMeter
+Invoke-WebRequest -Uri 'https://dlcdn.apache.org/jmeter/binaries/apache-jmeter-5.4.1.zip' -OutFile 'c:\temp\jmeter.zip'
 
 #InstallFSLogix
 Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
-Start-Sleep -Seconds 10
 Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
 Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
